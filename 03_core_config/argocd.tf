d479a44345bb488236eee8d093ae7d35105119d2
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2023-05-19 22:32:51 +1000

    Ensure correct context is set

--- 03_core_config/argocd.tf
+++ 03_core_config/argocd.tf
@@ -5,16 +5,17 @@ resource "null_resource" "add_cluster" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<-EOT
-        POD_ID=$(kubectl get pod -n argocd | grep 'argocd-server' | awk 'END {print $1}' | xargs echo)
+        POD_ID=$(kubectl --context $KUBECTL_CONTEXT get pod -n argocd | grep 'argocd-server' | awk 'END {print $1}' | xargs echo)
         CONTEXTS=$(yq '.clusters.[].name' ${local_file.new_kubeconfig.filename})
-        kubectl cp -n argocd ${local_file.new_kubeconfig.filename} $POD_ID:/tmp
+        kubectl --context $KUBECTL_CONTEXT cp -n argocd ${local_file.new_kubeconfig.filename} $POD_ID:/tmp
         for context in $CONTEXTS
         do
-            kubectl -n argocd exec $POD_ID -- argocd cluster add $context --name $context --kubeconfig /tmp/incluster_kubeconfig
+            kubectl --context $KUBECTL_CONTEXT -n argocd exec $POD_ID -- argocd cluster add $context --name $context --kubeconfig /tmp/incluster_kubeconfig
         done
     EOT
     environment = {
-      "KUBECONFIG" = "${local.k8s_config_dir}/config"
+      "KUBECONFIG"      = "${local.k8s_config_dir}/config"
+      "KUBECTL_CONTEXT" = "kind-management-cluster"
     }
   }
 
