d449b156cf33382ad4a66032a1ce5f33077a9425
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2023-05-19 22:55:12 +1000

    Login to argocd first

--- 03_core_config/argocd.tf
+++ 03_core_config/argocd.tf
@@ -8,6 +8,7 @@ resource "null_resource" "add_cluster" {
         POD_ID=$(kubectl --context $KUBECTL_CONTEXT get pod -n argocd | grep 'argocd-server' | awk 'END {print $1}' | xargs echo)
         CONTEXTS=$(yq '.clusters.[].name' ${local_file.new_kubeconfig.filename})
         kubectl --context $KUBECTL_CONTEXT cp -n argocd ${local_file.new_kubeconfig.filename} $POD_ID:/tmp
+        kubectl --context $KUBECTL_CONTEXT -n argocd exec $POD_ID -- sh -c 'echo "y" | argocd login localhost:8080 --username admin --password testpassword --insecure'
         for context in $CONTEXTS
         do
             kubectl --context $KUBECTL_CONTEXT -n argocd exec $POD_ID -- argocd cluster add $context --name $context --kubeconfig /tmp/incluster_kubeconfig
