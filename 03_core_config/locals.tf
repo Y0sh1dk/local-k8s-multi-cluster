25130474d797026adf8195b4ecfe4509dbacc39d
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2023-05-20 15:19:26 +1000

    Update locals names

--- 03_core_config/locals.tf
+++ 03_core_config/locals.tf
@@ -2,32 +2,30 @@ locals {
   k8s_config_dir = pathexpand("${path.cwd}/../contexts")
   tf_state_path  = pathexpand("${path.cwd}/../tf_state")
 
-  cluster_endpoints = [for cluster in data.terraform_remote_state.tf_01_clusters.outputs.cluster_configs : cluster.endpoint]
-
   kubeconfig_json = yamldecode(data.local_file.kubeconfig.content)
 
-  cluster_cluster_configs = { for cluster in local.kubeconfig_json.clusters : cluster.name => { address = cluster.cluster.server, certificate-authority-data = cluster.cluster.certificate-authority-data } }
-  cluster_user_configs    = { for user in local.kubeconfig_json.users : user.name => { client-certificate-data = user.user.client-certificate-data, client-key-data = user.user.client-key-data } }
+  kubeconfig_cluster_configs = { for cluster in local.kubeconfig_json.clusters : cluster.name => { address = cluster.cluster.server, certificate-authority-data = cluster.cluster.certificate-authority-data } }
+  kubeconfig_user_configs    = { for user in local.kubeconfig_json.users : user.name => { client-certificate-data = user.user.client-certificate-data, client-key-data = user.user.client-key-data } }
 
-  incluster_addresses = [for cluster in local.kubeconfig_json.clusters : merge(cluster, { "cluster" : { server : "https://${trimprefix(cluster.name, "kind-")}-control-plane:6443" } })]
+  incluster_kubeconfig_cluster_addresses = [for cluster in local.kubeconfig_json.clusters : merge(cluster, { "cluster" : { server : "https://${trimprefix(cluster.name, "kind-")}-control-plane:6443" } })]
 
-  new_clusters_block = [
-    for cluster in local.incluster_addresses : {
+  incluster_kubeconfig_clusters_block = [
+    for cluster in local.incluster_kubeconfig_cluster_addresses : {
       name = cluster.name
       cluster = {
-        certificate-authority-data = local.kubeconfig_json.clusters[index(local.incluster_addresses, cluster)].cluster.certificate-authority-data
+        certificate-authority-data = local.kubeconfig_json.clusters[index(local.incluster_kubeconfig_cluster_addresses, cluster)].cluster.certificate-authority-data
         server                     = cluster.cluster.server
     } }
   ]
 
-  incluster_kubeconfig = merge(local.kubeconfig_json, { clusters : local.new_clusters_block })
+  incluster_kubeconfig = merge(local.kubeconfig_json, { clusters : local.incluster_kubeconfig_clusters_block })
 
   cluster_credentials = {
-    for cluster in keys(local.cluster_cluster_configs) : cluster => {
-      address                    = local.cluster_cluster_configs[cluster].address
-      certificate-authority-data = local.cluster_cluster_configs[cluster].certificate-authority-data
-      client-certificate-data    = local.cluster_user_configs[cluster].client-certificate-data
-      client-key-data            = local.cluster_user_configs[cluster].client-key-data
+    for cluster in keys(local.kubeconfig_cluster_configs) : cluster => {
+      address                    = local.kubeconfig_cluster_configs[cluster].address
+      certificate-authority-data = local.kubeconfig_cluster_configs[cluster].certificate-authority-data
+      client-certificate-data    = local.kubeconfig_user_configs[cluster].client-certificate-data
+      client-key-data            = local.kubeconfig_user_configs[cluster].client-key-data
     }
   }
 
@@ -35,10 +33,10 @@ locals {
   ingress_nginx_config = data.terraform_remote_state.tf_02_core_services.outputs.ingress_nginx_config
 }
 
+################## Kubeconfigs ##################
 data "local_file" "kubeconfig" {
   filename = "${local.k8s_config_dir}/config"
 }
-
 
 resource "local_file" "incluster_kubeconfig" {
   content         = replace(yamlencode(local.incluster_kubeconfig), "/((?:^|\n)[\\s-]*)\"([\\w-]+)\":/", "$1$2:")
